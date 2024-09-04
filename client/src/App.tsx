ffeb47b1719197c081e7b5a307ca2b878ceffdcd
larrylenw <larrylenw@gmail.com>
2024-09-04 23:47:04 +0700

    created structure client

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+    return (
+           <div id='wrapper'>
+            "something"
+           </div>
+    )
+}
+
+export default App;
---
+++ client/src/main.tsx
@@ -0,0 +1,10 @@
+import './style.css'
+import { BrowserRouter as Router } from 'react-router-dom'
+import ReactDOM from 'react-dom/client'
+import App from './app'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <Router>
+          <App />
+  </Router>
+)
